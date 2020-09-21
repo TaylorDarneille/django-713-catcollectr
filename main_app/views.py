1b583e59127ee76f57fd515b2fde3f6e6fdf4bdb
Taylor Darneille <tmdarneille@gmail.com>
2020-09-21 11:05:49 -0700

    create and update

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 
 # Create your views (these are like your controller actions) here.
+# django will make a create cat form for us!
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form): # this will allow us to catch the pk to redirect to the show page
+        self.object = form.save(commit=False) # don't post to the db until we say so
+        self.object.save()
+        return HttpResponseRedirect('/cats/'+str(self.object.pk))
+
 def index(request):
     return render(request, 'index.html')
 
